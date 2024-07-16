6d0e56a67bb5540f0aa5da0552920467d6d801c1
Michael Walker <ouisihai@gmail.com>
2024-07-16 17:23:09 -0500

    feat(page): Adding in the 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,49 +1,23 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import {Button, Container, Group, Title, Text} from "@mantine/core";
+import * as classes from "../sass/pages/NotFound.module.css";
 
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
+const PageMissing = () => {
+    return (
+        <Container className={classes.root}>
+            <div className={classes.label}>404</div>
+            <Title className={classes.title}>You have found a secret place.</Title>
+            <Text c="dimmed" size="lg" ta="center" className={classes.description}>
+                Unfortunately, this is only a 404 page. You may have mistyped the address, or the page has
+                been moved to another URL.
+            </Text>
+            <Group justify="center">
+                <Button variant="subtle" size="md">
+                    Take me back to home page
+                </Button>
+            </Group>
+        </Container>
+    );
+};
 
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-const NotFoundPage = () => {
-  return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
-}
-
-export default NotFoundPage
-
-export const Head = () => <title>Not found</title>
+export default PageMissing;
