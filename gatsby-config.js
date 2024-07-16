a754585c69f59e0fa1ea1d941f1179250cd0d08f
Michael Walker <ouisihai@gmail.com>
2024-07-15 21:54:45 -0500

    chore(config): adding config files

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,34 +2,39 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  siteMetadata: {
-    title: `new-portfolio`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      "accessToken": "VsZKKPWtLzvlWDL6My6xBoVB0RRrCAdphx77Cp2SPMk",
-      "spaceId": "ubeasq4jx59j"
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-sass", "gatsby-plugin-google-gtag", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-transformer-remark", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+    siteMetadata: {
+        title: `new-portfolio`,
+        siteUrl: `https://www.yourdomain.tld`
     },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
+    plugins: [
+        {
+            resolve: 'gatsby-source-contentful',
+            options: {
+                "accessToken": process.env.access_token,
+                "spaceId": process.env.space_id
+            }
+        },
+        "gatsby-plugin-sharp",
+        "gatsby-plugin-postcss",
+        "gatsby-plugin-image",
+        "gatsby-transformer-sharp",
+        "gatsby-plugin-sass",
+        "gatsby-plugin-sitemap",
+        "gatsby-transformer-remark",
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                "name": "images",
+                "path": "./src/images/"
+            },
+            __key: "images"
+        },
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                "name": "pages",
+                "path": "./src/pages/"
+            },
+            __key: "pages"
+        }]
 };
